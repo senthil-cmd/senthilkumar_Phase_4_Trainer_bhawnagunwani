3144fd73b57e41976e4e979a8e41709544f99c78
Senthilkumar <34963570+senthil-cmd@users.noreply.github.com>
2021-06-21 19:43:18 +0530

    added backend framework spring boot
    backend springboot rest service and to perform curd operation in mongo db

---
+++ Backend-springboot-mongodb/KitchenStory/src/test/java/com/backend/KitchenStory/KitchenStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.KitchenStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
